86380cbd09f2b89b8367d736eefa70696cea2e4f
John Teckemeyer <johnteckemeyer@gmail.com>
2021-12-13 20:32:18 -0800

    Scratch the lines all together. Make the active node extend further out from the rest

--- src/components/FetchData.jsx
+++ src/components/FetchData.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 
 import './FetchData.css';
+import useWindowDimensions from './useWindowDimensions';
 
 const rocketColors = {
     'Falcon 1': '#d86628',
@@ -13,6 +14,7 @@ export default function FetchData (props) {
 
     const [data, setData] = useState([]);
     const [activeNode, setActiveNode] = useState(null);
+    const { width, height } = useWindowDimensions();
 
     const svgRef = useRef();
 
@@ -29,7 +31,7 @@ export default function FetchData (props) {
 
     // Hide launch details
     function handleHideDetails () {
-        setActiveNode(null);
+        setActiveNode(0);
     }
 
     useEffect(() => {
@@ -37,6 +39,7 @@ export default function FetchData (props) {
     }, [])
 
     useEffect(() => {
+        console.log(activeNode, '--------')
         const svg = d3.select(svgRef.current);
 
         const circleRadius = 10;
@@ -93,14 +96,19 @@ export default function FetchData (props) {
 
             updateLines(lines);
             updateCircles(circles);
+            updateTexts(texts);
 
             d3.select(this)
                 .select('line')
-                    .attr('x2', lineLength * 3)
+                    .attr('x2', lineLength + circleRadius * 6)
             
             d3.select(this)
                 .select('circle')
-                    .style('visibility', 'hidden')
+                    .attr('cx', lineLength + circleRadius * 6)
+            
+            d3.select(this)
+                .select('text')
+                    .attr('x', lineLength + circleRadius * 6)
         }
 
         // Draw the vertical axis
@@ -134,20 +142,13 @@ export default function FetchData (props) {
         // Draw numbers in the circles
         const texts = nodes
             .append('text')
-                .attr('class', 'launch-number')
-                .attr('font-size', '18px')
-                .attr('x', lineLength + circleRadius)
-                .attr('y', d => y(new Date(d.launch_date_utc)))
-                .style('font-size', '0px')
-                .attr('text-anchor', 'middle')
-                .attr('alignment-baseline', 'central')
-                .style('visibility', 'hidden')
-                .text(d => d.flight_number)
+        
+        updateTexts(texts);
 
         function updateCircles (c) {
             c
                 .attr('id', (d, i) => 'circle-' + i)
-                .attr('cx', lineLength + circleRadius)
+                .attr('cx', (d, i) => lineLength + circleRadius + (i % 2 ? circleRadius * 2 : 0))
                 .attr('cy', d => y(new Date(d.launch_date_utc)))
                 .attr('r', circleRadius)
                 .style('fill', d => rocketColors[d.rocket.rocket_name])
@@ -160,11 +161,24 @@ export default function FetchData (props) {
                 .attr('class', 'line-extension')
                 .attr('x1', 100)
                 .attr('y1', d=> y(new Date(d.launch_date_utc)))
-                .attr('x2', lineLength)
+                .attr('x2', (d, i) => lineLength + (i % 2 ? circleRadius * 2 : 0))
                 .attr('y2', d=> y(new Date(d.launch_date_utc)))
         }
 
-    }, [data])
+        function updateTexts (t) {
+            t
+                .attr('class', 'launch-number')
+                .attr('font-size', '18px')
+                .attr('x', (d, i) => lineLength + circleRadius + (i % 2 ? circleRadius * 2 : 0))
+                .attr('y', d => y(new Date(d.launch_date_utc)))
+                .style('font-size', '0px')
+                .attr('text-anchor', 'middle')
+                .attr('alignment-baseline', 'central')
+                .style('visibility', 'hidden')
+                .text(d => d.flight_number)
+        }
+
+    }, [data, activeNode])
 
     return (
         <div id="timeline-container">
